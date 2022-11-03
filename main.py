2c3323157a280d81afde6b3f63b266d26676f561
Chyna Gvng <angoyewally@outlook.com>
2022-11-03 12:45:48 +0300

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,43 @@
-# Python program to match women and their qualifications to jobs and opportunities
+# Import modules
+import sys
+
+# Ask user to input name
+print("Enter first name: ")
+first_name = input()
+print("Enter second name: ")
+second_name = input()
+
+# Ask user to select gender
+def gender_menu_choice():
+    def print_gender_menu():
+        print(30 * "-", "GENDER MENU", 30 * "-")
+        print("1. Male ")
+        print("2. Female ")
+        print(73 * "-")
+
+    loop = True
+
+    while loop:
+        print_gender_menu()
+        choice = input("Enter your choice [1-2]: ")
+
+        if choice == '1':
+            print("The platform is only available to women!")
+            loop = False
+            sys.exit()
+        elif choice == '2':
+            loop = False
+        else:
+            input("Wrong menu selection. Enter any key to try again..")
+    return None       
+print(gender_menu_choice())
+
+# Ask user to input age
+print("Enter your age: ")
+age = int(input())
+if age < 18:
+    print("You are under age")
+elif age > 45:
+        print("You are over age")
+
+# Ask user to input email
